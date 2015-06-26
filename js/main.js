3e847028e96aa1d03c19b3d62651b49d7ac7dd03
Great-Alexey <alexeyvax1990@gmail.com>
2015-06-26 23:01:39 +0300

    nazvanie sohranennih izmeneniy

--- js/main.js
+++ js/main.js
@@ -1,33 +1,46 @@
 
-a= $('nav a');
-function active(){
-    for(i=0;i<a.length;i++){
-        if(a[i]===this){
-            a[i].className='active';
+//a= $('nav a');
+//function active(){
+//    for(i=0;i<a.length;i++){
+//        if(a[i]===this){
+//            a[i].className='active';
+//        }
+//        else{
+//            a[i].className=' ';
+//        } 
+//    }
+//}
+//for(i=0;i<a.length;i++){
+//    a[i].addEventListener("click", active, false);
+//}
+//
+//
+
+margin = 0;
+$('#right1').click(function(){
+    var a= $('.carouselCont');
+    containerWidth = a.width();
+if(margin > -containerWidth){
+        margin= margin - 340;
+        $('.carouselCont').css({'margin-left': margin});
         }
-        else{
-            a[i].className=' ';
-        } 
-    }
-}
-for(i=0;i<a.length;i++){
-    a[i].addEventListener("click", active, false);
-}
-
-
-var i=0;
-$('.right').click(function(){
-    if(i===0){
-        $('.carouselCont').css({'margin-left': '-340px'});
-        
-        $('#right1').css({display: 'none'});
-        $('#left12').css({display: 'block'});
-        i=1;
-    }
-        else{
-            $('.carouselCont').css({'margin-left': '0px'});
-            $('#left12').css({display: 'none'});
-            $('#right1').css({display: 'block'});           
-            i=0;
+//        $('#right1').css({display: 'none'});
+//        $('#left12').css({display: 'block'});
+//        i=1;
+//    
+//        
+//            
+////            $('#left12').css({display: 'none'});
+////            $('#right1').css({display: 'block'});           
+//        
+});
+$('#left1').click(function(){
+    var a= $('.carouselCont');
+    containerWidth = a.width();
+    
+   if(margin > + containerWidth) {
+            margin= margin + 340;
+            $('.carouselCont').css({'margin-left': margin});
         }
 });
+
